0fec18b5c16e83c1fd2ab021f0818e9ba4184230
Josh Durgin <josh.durgin@inktank.com>
2013-03-31 16:51:30 -0700

    Mutex: include address in name
    This way lockdep can track individual locks more accurately,
    instead of treating all instances of the same usage of lock
    as the same lock.
    
    Signed-off-by: Josh Durgin <josh.durgin@inktank.com>

--- src/common/Mutex.cc
+++ src/common/Mutex.cc
@@ -17,14 +17,15 @@
 #include "common/perf_counters.h"
 #include "common/ceph_context.h"
 #include "common/config.h"
+#include "include/stringify.h"
 #include "include/utime.h"
 #include "common/Clock.h"
 
 Mutex::Mutex(const char *n, bool r, bool ld,
 	     bool bt,
 	     CephContext *cct) :
-  name(n), id(-1), recursive(r), lockdep(ld), backtrace(bt),
-  nlock(0), locked_by(0), cct(cct), logger(0)
+  name(string(n) + stringify(this)), id(-1), recursive(r), lockdep(ld),
+  backtrace(bt), nlock(0), locked_by(0), cct(cct), logger(0)
 {
   if (cct) {
     PerfCountersBuilder b(cct, string("mutex-") + name,
